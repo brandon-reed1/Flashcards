867e596737742019326f5955efdc7c106b1f3729
save-time <savetime929@gmail.com>
2021-05-13 11:54:10 -0400

    Refactor terminology use in forEach

--- src/Game.js
+++ src/Game.js
@@ -10,7 +10,7 @@ class Game {
   }
   start(cards) {
     const stack = []
-    cards.forEach(element => stack.push(element));
+    cards.forEach(card => stack.push(card));
     const deck = new Deck(stack);
     const round = new Round(deck);
     const game = new Game(round);
