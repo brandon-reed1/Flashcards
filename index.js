f58fe00d3228a683d272cf2eea8b44169550793a
save-time <savetime929@gmail.com>
2021-05-12 22:23:36 -0400

    Remove unused require

--- index.js
+++ index.js
@@ -1,7 +1,6 @@
 const Game = require('./src/Game');
 const data = require('./src/data');
 const prototypeQuestions = data.prototypeData;
-// const util = require('./util');
 
 const http = require('http');
 let app = http.createServer();
@@ -10,5 +9,5 @@ let app = http.createServer();
 app.listen(3000, '127.0.0.1');  
 console.log('Node server running on port 3000'); 
 
-const game = new Game();
+let game = new Game();
 game.start(prototypeQuestions)
